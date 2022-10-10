c4e72ee9ed9f0f277420a1ac95cd26dc5cd28b5d
Alvaro <alvaro.guillenc1612@gmail.com>
2022-10-09 22:51:08 -0500

    no github

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import express from "express";
-import { routes } from "./routes/router.js";
-const app = express();
-const port = 3002;
+import express from "express"
+import { routes } from "./routes/router.js"
+const app = express()
+const port = 3002
 
-app.use(routes);
+app.use(routes)
 
 app.listen(port, () => {
     console.log(`Aplicación en el puerto ${port}`)
