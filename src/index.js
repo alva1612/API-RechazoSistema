7109c28087c6371155cd890588ec5656c123e93f
Alvaro <alvaro.guillenc1612@gmail.com>
2022-10-11 22:35:49 -0500

    railway host and port setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import express from "express"
 import { routes } from "./routes/router.js"
 const app = express()
+const host = "0.0.0.0"
 const port = 3002
+
+const options = {
+    host,
+    port
+}
 
 app.use(routes)
 
-app.listen(port, () => {
+
+
+app.listen(options, () => {
     console.log(`Aplicación en el puerto ${port}`)
 })
 
