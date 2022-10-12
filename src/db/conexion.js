9fff7e3dd0193bc83ddd941d807ea098b26ad880
Alvaro <alvaro.guillenc1612@gmail.com>
2022-10-11 22:22:47 -0500

    conection to railway bd

--- src/db/conexion.js
+++ src/db/conexion.js
@@ -1,14 +1,14 @@
 import mysql from 'mysql2'
 
-/* const connection = mysql.createConnection({
-    host: 'localhost',
+const connection = mysql.createConnection({
+    host: 'containers-us-west-100.railway.app:7701',
     user: 'root',
-    database: 'sistema_empleo',
-    password: 'root'
+    database: 'railway',
+    password: '5lvCuG4HnfofmcYra9g7'
 });
 
 
 
 
 
-export {connection}; */
+export {connection};
--- src/routes/router.js
+++ src/routes/router.js
@@ -2,10 +2,10 @@ import { Router } from "express";
 import pkg from "body-parser";
 
 import { mainController } from "../Controller/mainController.js";
-/* import { empleosController } from "../Controller/empleoController.js";
+import { empleosController } from "../Controller/empleoController.js";
 import { empleadorController } from "../Controller/empleadorController.js";
 import { buscarEmpleador } from "../controller/utilsController.js";
-import { postulacionController } from "../Controller/postulacionController.js"; */
+import { postulacionController } from "../Controller/postulacionController.js";
 
 const routes = Router();
 const { urlencoded } = pkg;
@@ -15,17 +15,17 @@ const urlEncoder = urlencoded({extended: true});
 routes.get("/", mainController.index)
 
 //METODOS RELACIONADOS AL EMPLEADOR
-/* routes.get("/empleador",empleadorController.listar); */
+routes.get("/empleador",empleadorController.listar);
 
-/* routes.get("/buscar-empleador", (req, res) => buscarEmpleador(req.query.id)); */
-/* routes.post("/empleador", urlEncoder, empleadorController.agregar); */
+routes.get("/buscar-empleador", (req, res) => buscarEmpleador(req.query.id));
+routes.post("/empleador", urlEncoder, empleadorController.agregar);
 
 //METODOS RELACIONADOS AL EMPLEO
-/* routes.get("/empleo", empleosController.listar); */
-/* routes.post("/empleo", urlEncoder, empleosController.agregar); */
+routes.get("/empleo", empleosController.listar);
+routes.post("/empleo", urlEncoder, empleosController.agregar);
 
 //METODOS RELACIONADOS A LA POSTULACION
-/* routes.get("/postulaciones", postulacionController.listar ); */
-/* routes.post('/postular', urlEncoder, postulacionController.postular); */
+routes.get("/postulaciones", postulacionController.listar );
+routes.post('/postular', urlEncoder, postulacionController.postular);
 
 export {routes};
